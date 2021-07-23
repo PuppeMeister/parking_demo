5f009173c6587a3ee7eed36cc7b55971bad19f4e
PuppeMeister <puppe.meister@gmail.com>
2021-07-23 19:26:02 +0900

    Initial Commit

---
+++ src/test/java/com/example/parking_demo/ParkingDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.parking_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
